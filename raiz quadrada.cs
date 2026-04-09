c630736c467472a48fee66ce7fe9b04efb99f49e
giohzxz <giovannapereiradesousa@gmail.com>
2026-04-09 09:35:59 -0300

    atividades

---
+++ raiz quadrada.cs
@@ -0,0 +1,16 @@
+﻿// Nome:GIOVANNA
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        double numero = double.Parse(Console.ReadLine());
+
+        double raiz = Math.Sqrt(numero);
+
+        Console.WriteLine("A raiz quadrada é: " + raiz);
+    }
+}
